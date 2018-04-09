2d9b6e8a1f98c13ad72904eab5d59dfb0a2e64b7
KIMMINJAE <innersme@gmail.com>
2018-04-09 12:09:37 +0900

    Initial Commit

---
+++ OSP_PNU2018/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OSP_PNU2018
+//
+//  Created by MJMacBook on 09/04/2018.
+//  Copyright © 2018 MJMacBook. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
