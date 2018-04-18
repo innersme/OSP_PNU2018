67283929926235b36b4ef53cb19634ab7c82f8a7
MJKimlinux <innersme@gmail.com>
2018-04-18 13:03:55 +0900

    mv HW Folder

---
+++ OSP_PNU2018/CH4/makefile/sum.h
@@ -0,0 +1,14 @@
+//
+//  sum.h
+//  OSP_PNU2018
+//
+//  Created by MJMacBook on 18/04/2018.
+//  Copyright © 2018 MJMacBook. All rights reserved.
+//
+
+#ifndef sum_h
+#define sum_h
+
+int sum(int a, int b);
+
+#endif /* sum_h */
