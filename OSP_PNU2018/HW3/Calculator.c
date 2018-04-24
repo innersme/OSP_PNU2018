ba1033f13e3b3545828017e2afbb342e66ecf43f
KIMMINJAE <innersme@gmail.com>
2018-04-24 18:38:10 +0900

    HW3

--- OSP_PNU2018/HW3/Calculator.c
+++ OSP_PNU2018/HW3/Calculator.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "calculator.h"
 
 enum{Add=1, Minus, Multi, Divide, Exit};
 
-int main()
+int main(int argc, char** argv)
 {
     double val1, val2;
     int select;
     double result = 0.0;
-    while (1) {
-        printf("Enter val1 and val2:");
-        scanf("%lf %lf", &val1, &val2);
-        printf("Enter calculator mode(1:Add, 2:Minus, 3:Multi, 4:Divide, 5:Exit)");
-        scanf("%d", &select);
+
+    if (argc < 4) {
+        printf("!!!More arguments needed! ");
+        return 0;
+    }
+    else if (argc > 4){
+        printf("!!!Too many arguments. ");
+        return 0;
+    }
+    else
+    {
+        val1 = atoi(argv[1]);
+        val2 = atoi(argv[2]);
+        select = atoi(argv[3]);
+        
+        //printf("%lf %lf %d", val1, val2, select );
         switch (select) {
             case Add:
             {
@@ -38,13 +50,8 @@ int main()
             {
                 return 0;
             }
-            default:
-            {
-                printf("repeat the other value");
-                break;
-            }
         }
         printf("Result value: %lf\n", result);
+        return 0;
     }
-    return 0;
 }
